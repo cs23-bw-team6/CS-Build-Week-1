0785d5fc079e3757ca1a2dd0bfc08c6224ca8656
Paulus Kutrich <pkutrich@gmail.com>
2019-11-18 11:04:38 -0800

    Linting

--- adventure/api.py
+++ adventure/api.py
@@ -8,8 +8,13 @@ from .models import *
 from rest_framework.decorators import api_view
 import json
 
+
 # instantiate pusher
-# pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
+# pusher = Pusher(app_id=config('PUSHER_APP_ID'),
+#                 key=config('PUSHER_KEY'),
+#                 secret=config('PUSHER_SECRET'),
+#                 cluster=config('PUSHER_CLUSTER'))
+
 
 @csrf_exempt
 @api_view(["GET"])
@@ -19,14 +24,16 @@ def initialize(request):
     player_id = player.id
     uuid = player.uuid
     room = player.room()
-    players = room.playerNames(player_id)
-    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
+    players = room.player_names(player_id)
+    return JsonResponse(
+        {'uuid': uuid, 'name': player.user.username, 'title': room.title, 'description': room.description,
+         'players': players}, safe=True)
 
 
 # @csrf_exempt
 @api_view(["POST"])
 def move(request):
-    dirs={"n": "north", "s": "south", "e": "east", "w": "west"}
+    dirs = {"n": "north", "s": "south", "e": "east", "w": "west"}
     reverse_dirs = {"n": "south", "s": "north", "e": "west", "w": "east"}
     player = request.user.player
     player_id = player.id
@@ -34,34 +41,40 @@ def move(request):
     data = json.loads(request.body)
     direction = data['direction']
     room = player.room()
-    nextRoomID = None
+    next_room_id = None
     if direction == "n":
-        nextRoomID = room.n_to
+        next_room_id = room.n_to
     elif direction == "s":
-        nextRoomID = room.s_to
+        next_room_id = room.s_to
     elif direction == "e":
-        nextRoomID = room.e_to
+        next_room_id = room.e_to
     elif direction == "w":
-        nextRoomID = room.w_to
-    if nextRoomID is not None and nextRoomID > 0:
-        nextRoom = Room.objects.get(id=nextRoomID)
-        player.currentRoom=nextRoomID
+        next_room_id = room.w_to
+    if next_room_id and next_room_id > 0:
+        next_room = Room.objects.get(id=next_room_id)
+        player.currentRoom = next_room_id
         player.save()
-        players = nextRoom.playerNames(player_id)
-        currentPlayerUUIDs = room.playerUUIDs(player_id)
-        nextPlayerUUIDs = nextRoom.playerUUIDs(player_id)
-        # for p_uuid in currentPlayerUUIDs:
-        #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
-        # for p_uuid in nextPlayerUUIDs:
-        #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
-        return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
+        players = next_room.player_names(player_id)
+        current_player_uui_ds = room.player_UUIDs(player_id)
+        next_player_uui_ds = next_room.player_UUIDs(player_id)
+        # for p_uuid in current_player_uui_ds:
+        #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast',
+        #                    {'message': f'{player.user.username} has walked {dirs[direction]}.'})
+        # for p_uuid in next_player_uui_ds:
+        #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast',
+        #                    {'message': f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
+        return JsonResponse(
+            {'name': player.user.username, 'title': next_room.title, 'description': next_room.description,
+             'players': players, 'error_msg': ""}, safe=True)
     else:
-        players = room.playerNames(player_id)
-        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        players = room.player_names(player_id)
+        return JsonResponse(
+            {'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players,
+             'error_msg': "You cannot move that way."}, safe=True)
 
 
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    return JsonResponse({'error': "Not yet implemented"}, safe=True, status=500)
--- adventure/models.py
+++ adventure/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 import uuid
 
+
 class Room(models.Model):
     title = models.CharField(max_length=50, default="DEFAULT TITLE")
     description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION")
@@ -12,39 +13,50 @@ class Room(models.Model):
     s_to = models.IntegerField(default=0)
     e_to = models.IntegerField(default=0)
     w_to = models.IntegerField(default=0)
-    def connectRooms(self, destinationRoom, direction):
-        destinationRoomID = destinationRoom.id
+
+    def __str__(self):
+        return f"{self.title}\n{self.description}"
+
+    def connect_rooms(self, destination_room, direction):
+        destination_room_id = destination_room.id
         try:
-            destinationRoom = Room.objects.get(id=destinationRoomID)
+            destination_room = Room.objects.get(id=destination_room_id)
         except Room.DoesNotExist:
             print("That room does not exist")
         else:
             if direction == "n":
-                self.n_to = destinationRoomID
+                self.n_to = destination_room_id
             elif direction == "s":
-                self.s_to = destinationRoomID
+                self.s_to = destination_room_id
             elif direction == "e":
-                self.e_to = destinationRoomID
+                self.e_to = destination_room_id
             elif direction == "w":
-                self.w_to = destinationRoomID
+                self.w_to = destination_room_id
             else:
                 print("Invalid direction")
                 return
             self.save()
-    def playerNames(self, currentPlayerID):
-        return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
-    def playerUUIDs(self, currentPlayerID):
-        return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+
+    def player_names(self, current_player_ID):
+        return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(current_player_ID)]
+
+    def player_UUIDs(self, current_player_ID):
+        return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(current_player_ID)]
 
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     currentRoom = models.IntegerField(default=0)
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_login}"
+
     def initialize(self):
         if self.currentRoom == 0:
             self.currentRoom = Room.objects.first().id
             self.save()
+
     def room(self):
         try:
             return Room.objects.get(id=self.currentRoom)
@@ -52,11 +64,13 @@ class Player(models.Model):
             self.initialize()
             return self.room()
 
+
 @receiver(post_save, sender=User)
 def create_user_player(sender, instance, created, **kwargs):
     if created:
         Player.objects.create(user=instance)
         Token.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_player(sender, instance, **kwargs):
