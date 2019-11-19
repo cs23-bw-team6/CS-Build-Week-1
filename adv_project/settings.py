6af5d7c2cf3e2621dfac122d55878a76e5d6161a
Will Haeck <will.haeck@gmail.com>
2019-11-19 15:11:37 -0800

    changed static url and removed Archive.zip"

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/client/build/'
 
 import django_heroku
 django_heroku.settings(locals())
