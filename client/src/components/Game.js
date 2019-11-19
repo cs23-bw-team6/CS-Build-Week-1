427f132495940414fe5843e6e3b55aea26ba35da
Will Haeck <will.haeck@gmail.com>
2019-11-19 10:44:07 -0800

    just files really.

---
+++ client/src/components/Game.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Map from 'Map.js';
+
+const Game = () => {
+    return (<div>
+        <header>LambdaMUD</header>
+        <div>
+            <nav></nav>
+            <Map />
+        </div>
+        
+    </div>);
+}
+
+export default Game;
