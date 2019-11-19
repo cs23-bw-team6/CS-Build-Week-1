22b0f97da8d63a6faad5b9518cbe8c083e3ffbe4
Paulus Kutrich <pkutrich@gmail.com>
2019-11-19 15:04:42 -0800

    Make diamond pattern

--- util/sample_generator.py
+++ util/sample_generator.py
@@ -4,39 +4,9 @@
 # You can modify generate_rooms() to create your own
 # procedural generation algorithm and use print_rooms()
 # to see the world.
-
-
-class Room:
-    def __init__(self, id, name, description, x, y):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.n_to = None
-        self.s_to = None
-        self.e_to = None
-        self.w_to = None
-        self.x = x
-        self.y = y
-
-    def __repr__(self):
-        if self.e_to is not None:
-            return f"({self.x}, {self.y}) -> ({self.e_to.x}, {self.e_to.y})"
-        return f"({self.x}, {self.y})"
-
-    def connect_rooms(self, connecting_room, direction):
-        """
-        Connect two rooms in the given n/s/e/w direction
-        """
-        reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-        reverse_dir = reverse_dirs[direction]
-        setattr(self, f"{direction}_to", connecting_room)
-        setattr(connecting_room, f"{reverse_dir}_to", self)
-
-    def get_room_in_direction(self, direction):
-        """
-        Connect two rooms in the given n/s/e/w direction
-        """
-        return getattr(self, f"{direction}_to")
+from adventure.models import Item, Container, Room
+from .name_generation import make_name
+from collections import deque
 
 
 class World:
@@ -46,10 +16,6 @@ class World:
         self.height = 0
 
     def generate_rooms(self, size_x, size_y, num_rooms):
-        """
-        Fill up the grid, bottom to top, in a zig-zag pattern
-        """
-
         # Initialize the grid
         self.grid = [None] * size_y
         self.width = size_x
@@ -57,45 +23,35 @@ class World:
         for i in range(len(self.grid)):
             self.grid[i] = [None] * size_x
 
-        # Start from lower-left corner (0,0)
-        x = -1  # (this will become 0 on the first step)
-        y = 0
-        room_count = 0
+        mid_y = size_y // 2
+        mid_x = size_x // 2
 
-        # Start generating rooms to the east
-        direction = 1  # 1: east, -1: west
-
-        # While there are rooms to be created...
-        previous_room = None
+        first = Room(title=make_name(), x=mid_x, y=mid_y)
+        first.save()
+        self.grid[first.y][first.x] = first.id
+        queue = deque()
+        queue.append(first)
+        print('I am first', first.id)
+        room_count = 1
         while room_count < num_rooms:
+            current_room = queue.popleft()
+            for direction, delta_x, delta_y in zip(['n', 'w', 's', 'e'], [0, 1, 0, -1], [-1, 0, 1, 0]):
+                if not eval(f'current_room.{direction}_to'):
+                    new_y = current_room.y + delta_y
+                    if not size_y > new_y >= 0:
+                        continue
 
-            # Calculate the direction of the room to be created
-            if direction > 0 and x < size_x - 1:
-                room_direction = "e"
-                x += 1
-            elif direction < 0 and x > 0:
-                room_direction = "w"
-                x -= 1
-            else:
-                # If we hit a wall, turn north and reverse direction
-                room_direction = "n"
-                y += 1
-                direction *= -1
+                    new_x = current_room.x + delta_x
+                    if not size_x > new_x >= 0:
+                        continue
 
-            # Create a room in the given direction
-            room = Room(room_count, "A Generic Room", "This is a generic room.", x, y)
-            # Note that in Django, you'll need to save the room after you create it
-
-            # Save the room in the World grid
-            self.grid[y][x] = room
-
-            # Connect the new room to the previous room
-            if previous_room is not None:
-                previous_room.connect_rooms(room, room_direction)
-
-            # Update iteration variables
-            previous_room = room
-            room_count += 1
+                    if not self.grid[new_y][new_x]:
+                        next_room = Room(title=make_name(), x=new_x, y=new_y)
+                        next_room.save()
+                        self.grid[new_y][new_x] = next_room.id
+                        current_room.connect_rooms(next_room, direction)
+                        queue.append(next_room)
+                        room_count += 1
 
     def print_rooms(self):
         """
@@ -115,7 +71,7 @@ class World:
             # PRINT NORTH CONNECTION ROW
             string += "#"
             for room in row:
-                if room is not None and room.n_to is not None:
+                if room is not None and Room.objects.filter(id=room)[0].n_to is not None:
                     string += "  |  "
                 else:
                     string += "     "
@@ -123,15 +79,15 @@ class World:
             # PRINT ROOM ROW
             string += "#"
             for room in row:
-                if room is not None and room.w_to is not None:
+                if room is not None and Room.objects.filter(id=room)[0].w_to is not None:
                     string += "-"
                 else:
                     string += " "
                 if room is not None:
-                    string += f"{room.id}".zfill(3)
+                    string += f"{Room.objects.filter(id=room)[0].id}".zfill(3)
                 else:
                     string += "   "
-                if room is not None and room.e_to is not None:
+                if room is not None and Room.objects.filter(id=room)[0].e_to is not None:
                     string += "-"
                 else:
                     string += " "
@@ -139,7 +95,7 @@ class World:
             # PRINT SOUTH CONNECTION ROW
             string += "#"
             for room in row:
-                if room is not None and room.s_to is not None:
+                if room is not None and Room.objects.filter(id=room)[0].s_to is not None:
                     string += "  |  "
                 else:
                     string += "     "
@@ -153,9 +109,9 @@ class World:
 
 
 w = World()
-num_rooms = 44
-width = 8
-height = 7
+num_rooms = 15
+width = 15
+height = 15
 w.generate_rooms(width, height, num_rooms)
 w.print_rooms()
 
