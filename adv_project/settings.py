1e8a2e2d8196ad7c7cfec66e3a661566cbeaabde
Will Haeck <will.haeck@gmail.com>
2019-11-19 15:18:48 -0800

    changed position of static root and static url

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -147,8 +147,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, '/client/build/')
 STATIC_ROOT = os.path.join(BASE_DIR, '/client/build/')
+STATIC_URL = os.path.join(BASE_DIR, '/client/build/')
 
 import django_heroku
 django_heroku.settings(locals())
