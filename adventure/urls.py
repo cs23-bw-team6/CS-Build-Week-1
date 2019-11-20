74907d28b11cca5d3890ecaee8f3c0fafc2d0067
Paulus Kutrich <pkutrich@gmail.com>
2019-11-19 23:13:30 -0800

    Add get_item

--- adventure/urls.py
+++ adventure/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import api
 
 urlpatterns = [
+    url('get_item', api.get_item),
     url('rooms', api.rooms),
     url('init', api.initialize),
     url('move', api.move),
