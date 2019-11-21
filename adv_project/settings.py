6af8b29b370d229c357c9229c9a3bfd074723f62
Will Haeck <will.haeck@gmail.com>
2019-11-21 13:06:45 -0800

    am i dumb?

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
