030450ba6e02d9a21f0d8c616490658b08ef6711
Will Haeck <will.haeck@gmail.com>
2019-11-21 07:17:05 -0800

    didnt add empty object first

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -28,6 +28,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 # Enable postgres?
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
