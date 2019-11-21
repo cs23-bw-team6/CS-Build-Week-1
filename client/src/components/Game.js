940afd87db228c9699201befa45072923c567ad6
Ian Be <ian84be@gmail.com>
2019-11-20 22:14:52 -0600

    buildout UI for compass nav buttons

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import oldMap from '../assets/map.png';
+import Compass from './Compass';
+import '../scss/Game.scss';
 
 const Game = () => {
-  return <section className="Game">GAME</section>;
+  return (
+    <section className="Game">
+      <header className="Game__header">
+        <img src={oldMap} alt="old map with scope and compass" />
+        <h1>ADVENTURE GAME</h1>
+      </header>
+
+      <main className="Game__body">
+        <section className="Game__body-left">
+          <div className="WorldMap">WorldMap</div>
+          <div className="Commo">Commo</div>
+        </section>
+        <section className="Game__body-right">
+          <div className="Inventory">Inventory</div>
+          <Compass />
+        </section>
+      </main>
+    </section>
+  );
 };
 
 export default Game;
