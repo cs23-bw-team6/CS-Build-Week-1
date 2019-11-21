c8693d34719824a5f3005985032f0df11a7324df
Will Haeck <will.haeck@gmail.com>
2019-11-21 13:02:36 -0800

    why

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -171,8 +171,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-STATIC_URL = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/client/public/')
+STATIC_URL = os.path.join(BASE_DIR, '/client/public/')
 
 LOGGING = {
     'version': 1,
