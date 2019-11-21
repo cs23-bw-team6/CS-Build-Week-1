242c486f064bef473babc4da836d165e5a50518a
Paulus Kutrich <pkutrich@gmail.com>
2019-11-21 00:02:32 -0800

    Add docstrings and comments.

--- util/name_generation.py
+++ util/name_generation.py
@@ -1,19 +1,20 @@
 import random
 
-room_types = ['Cave', 'Antechamber', 'Lanai', 'Armory', 'Den', 'Storeroom', 'Recroom', 'Kitchen',
+room_types = ['Cave', 'Antechamber', 'Lanai', 'Armory', 'Den', 'Storeroom', 'Rec-room', 'Kitchen',
               'Bedroom', 'Bathroom', 'Mud Room', 'Great Hall', 'Office', 'Library', 'Nursery', 'Dressing Room',
-              'Ballroom', 'Drawing Room', 'Sitting Room', 'Game Room']
-adjectives = ['Brutal', 'Frozen', 'Imposter', 'Invisible', 'Savage', 'Treacherous', 'Ruthless',
-              'Ferocious', 'Hapless', 'Traiterous', 'Perfidious', 'Duplicitous', 'Precarious', 'Malignant',
-              'Wicked', 'Menacing', 'Perilous']
-nouns = ['Orc', 'Phoenix', 'Army', 'Tombs', 'Elf', 'Goblin', 'Kobold', 'Dwarf', 'Gremlin',
-         'Halfling', 'Gnome', 'Giant', 'Treant', 'Dragonborn', 'Ent', 'Mermaid', 'Troll', 'Satyr',
+              'Ballroom', 'Drawing Room', 'Sitting Room', 'Game Room', 'Breakfast Nook', 'Study', 'Closet', 'Basement']
+adjectives = ['Brutal', 'Frozen', 'Impostor', 'Invisible', 'Savage', 'Treacherous', 'Ruthless',
+              'Ferocious', 'Hapless', 'Traitorous', 'Perfidious', 'Duplicitous', 'Precarious', 'Malignant',
+              'Wicked', 'Menacing', 'Perilous', 'Unscrupulous', 'Sneaky', 'Oozing', 'Scraggly', 'Odoriferous']
+nouns = ['Orc', 'Phoenix', 'Army', 'Tombs', 'Elf', 'Goblin', 'Kobold', 'Dwarf', 'Gremlin', 'Bolrog', 'Demigorgon',
+         'Halfling', 'Gnome', 'Giant', 'Treant', 'Dragon-born', 'Ent', 'Mermaid', 'Troll', 'Satyr', 'T-Rex',
          'Bandersnatch', 'Sphinx']
 
 
 def make_name():
     name = f"{random.choice(room_types)} of the {random.choice(adjectives)} {random.choice(nouns)}"
     return name
+
 
 def adj_noun():
     name = f"{random.choice(adjectives)} {random.choice(nouns)}"
--- util/world.py
+++ util/world.py
@@ -1,9 +1,7 @@
-# Sample Python code that can be used to generate rooms in
-# a zig-zag pattern.
-#
-# You can modify generate_rooms() to create your own
-# procedural generation algorithm and use print_rooms()
-# to see the world.
+"""Create a game world of interconnected rooms to navigate.
+Scatter treasure chests and keys throughout the world.
+Place treasure in one chest as the goal of the game."""
+
 from adventure.models import Item, Container, Room, Player
 from .name_generation import make_name, adj_noun
 from collections import deque
@@ -11,32 +9,52 @@ import random
 
 
 class World:
-    def __init__(self, size_x, size_y, num_rooms, num_chests):
-        self.grid = None
-        self.width = size_x
-        self.height = size_y
-        self.room_count = 1
+    """Define a game world with rooms connected on a 2D grid.
+
+    :var width: The width of the grid.
+    :var height: The height of the grid.
+    :var num_rooms: The number of rooms to add.
+    :var num_chests: The number of chests to hide.
+    """
+    def __init__(self, width, height, num_rooms, num_chests):
+        self.width = width
+        self.height = height
         self.num_rooms = num_rooms
         self.num_chests = num_chests
+        self.grid = None
+        self.room_count = 1
 
     def generate_rooms(self):
+        """Make a series of interconnected hallways radiating from a central point."""
         # Initialize the grid
         self.grid = [None] * self.height
         for i in range(len(self.grid)):
             self.grid[i] = [None] * self.width
 
+        # Start in the middle of the grid.
         mid_y = (self.height // 2)
         mid_x = (self.width // 2) - 1
 
+        # Make the first room to add to our world. Place it in the center of the grid.
         first = Room(title=make_name(), x=mid_x, y=mid_y)
         first.save()
         self.grid[first.y][first.x] = first.id
+
+        # Place room number one in a queue.
         queue = deque()
         queue.append(first)
+
+        # Keep making rooms until we've reached our desired number.
         while self.room_count < self.num_rooms:
             current_room = queue.popleft()
+
+            # Connect another room to the current one in each available direction.
             for direction, delta_x, delta_y in zip(['n', 'w', 's', 'e'], [0, 1, 0, -1], [-1, 0, 1, 0]):
+
+                # If there's already a room this direction, keep going.
                 if not eval(f'current_room.{direction}_to'):
+
+                    # Check that we're still on the grid.
                     new_y = current_room.y + delta_y
                     if not self.height > new_y >= 0:
                         continue
@@ -45,7 +63,11 @@ class World:
                     if not self.width > new_x >= 0:
                         continue
 
+                    # Check that this grid space is open.
                     if not self.grid[new_y][new_x]:
+                        # Make a new room, save it, connect it to the current room,
+                        # and add it into the queue so we can add more rooms to it.
+                        # TODO: Add room description.
                         next_room = Room(title=make_name(), x=new_x, y=new_y)
                         next_room.save()
                         self.grid[new_y][new_x] = next_room.id
@@ -54,10 +76,7 @@ class World:
                         self.room_count += 1
 
     def print_rooms(self):
-        """
-        Print the rooms in room_grid in ascii characters.
-        """
-
+        """Print the rooms in room_grid in ascii characters."""
         # Add top border
         string = "# " * ((3 + self.width * 5) // 2) + "\n"
 
@@ -103,15 +122,14 @@ class World:
         print(string)
 
     def create_items(self):
-        # pick a chest to put the treasure in
-
+        """Create chests and their keys in pairs with matching id."""
         for i in range(self.num_chests):
-            # create chest/key pairs
+            # Make a hilarious name for the pair.
             name = adj_noun()
             key_name = "Key of the " + name
             chest_name = "Chest of the " + name
             k = Item(name=key_name,
-                     description=f"Maybe it opens the {chest_name}!",
+                     description=f"Maybe it opens the {chest_name}?!",
                      room=None,
                      is_key=True)
             c = Container(name=chest_name,
@@ -123,9 +141,11 @@ class World:
 
 
 def seed_items(num_chests, num_rooms):
-    """Add keys and chests to random rooms.
+    """Move keys and chests to random rooms.
     Put the treasure in the lucky chest.
+    Remove keys from players.
     """
+    # Create the required number of chests and keys.
     for i in range(1, num_chests):
         key = Item.objects.get(id=i)
         chest = Container.objects.get(id=i)
@@ -136,6 +156,7 @@ def seed_items(num_chests, num_rooms):
         key.save()
         chest.save()
 
+    # Creat a treasure and hide it in a random chest.
     lucky_chest_number = random.randint(1, num_chests)
     the_treasure = Item(name="The Treasure",
                         description="That thing you want",
@@ -144,14 +165,15 @@ def seed_items(num_chests, num_rooms):
 
 
 def seed_players(num_rooms):
-    players = Player.objects.all()
+    """Move all players to a random room to start the hunt again."""
     start_room = random.randint(1, num_rooms)
-    for p in players:
-        p.currentRoom = start_room
-        p.save()
+    for player in Player.objects.all():
+        player.current_room = start_room
+        player.save()
 
 
 def main():
+    """Run script to populate database with rooms and items."""
     num_rooms = 10
     width = 5
     height = 5
