d7157775fc0640d35fae41f42d1bafff8c7a967b
Ian Be <ian84be@gmail.com>
2019-11-20 20:44:41 -0600

    restructure app and scaffold Game component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import Login from './components/Login';
-import oldMap from './assets/map.png';
+import Game from './components/Game';
 import './App.scss';
 
 function App() {
-	return (
-		<div className="App">
-			<section className="left-side">
-				<h1>ADVENTURE GAME</h1>
-				<img src={oldMap} alt="old map with scope and compass" />
-			</section>
-			<Login />
-		</div>
-	);
+  const token = window.localStorage.getItem('token');
+  return <div className="App">{token ? <Game /> : <Login />}</div>;
 }
 
 export default App;
---
+++ client/src/components/Game.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Game = () => {
+  return <section className="Game">GAME</section>;
+};
+
+export default Game;
