be28ecb7b084f0a291f1e18bc2d867340c13dc13
Will Haeck <will.haeck@gmail.com>
2019-11-21 22:35:34 -0800

    react django

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'api',
     'corsheaders',
     'rest_framework',
+    'frontend',
     'rest_framework.authtoken',
     'rest_auth',
     'django.contrib.sites',
@@ -89,12 +90,12 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
